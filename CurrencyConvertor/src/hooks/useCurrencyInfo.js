69323df89af7abdd87352008cd0b903d29998d8a
Utkarsh Mishra <utkarshmishra0708@gmail.com>
2025-11-10 08:38:13 +0530

    final

---
+++ CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]))
+      .catch((err) => console.error("Error fetching currency info:", err));
+    console.log(data);
+  }, [currency]);
+  console.log("hello", data);
+
+  return data;
+}
+
+export default useCurrencyInfo;
